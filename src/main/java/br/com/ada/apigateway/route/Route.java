448c17de30d613e986da8fb78d0c155b989677e2
Alex Araujo <alexsaspbr@gmail.com>
2023-09-14 09:16:48 -0300

    criando container docker

--- src/main/java/br/com/ada/apigateway/route/Route.java
+++ src/main/java/br/com/ada/apigateway/route/Route.java
@@ -1,5 +1,6 @@
 package br.com.ada.apigateway.route;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Route {
 
+    @Value("${host.products}")
+    private String hostProducts;
+    @Value("${host.shop}")
+    private String hostShop;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("products_route", r -> r.path("/products/**")
-                        .uri("http://localhost:8080"))
+                        .uri(hostProducts))
                 .route("shop_route", r -> r.path("/shop/**")
-                        .uri("http://localhost:8082"))
+                        .uri(hostShop))
                 .build();
     }
 
